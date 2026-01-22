8f5046dc7f8997ac0015ddf04ba5906a644ad9df
Omer Mecitoglu <omer.mecitoglu@gmail.com>
2026-01-22 17:41:42 +0700

    refactor: displayMessage with custom message type

--- src/core/displayMessage.test.ts
+++ src/core/displayMessage.test.ts
@@ -2,14 +2,26 @@ import { describe, expect, it } from "vitest";
 import { displayMessage } from "./displayMessage";
 
 describe("displayMessage", () => {
-  it("should throw an object", () => {
+  it("should ", () => {
     try {
-      displayMessage("warning", "Unauthorized access");
+      displayMessage("username", "USERNAME_TAKEN");
     } catch (error) {
       expect(error).toEqual({
         isDisplayMessage: true,
-        messageType: "warning",
-        message: "Unauthorized access",
+        messageType: "username",
+        message: "USERNAME_TAKEN",
+      });
+    }
+  });
+
+  it('should wrap "error", "warning" and "success" message types with square brackets', () => {
+    try {
+      displayMessage("error", "UNAUTHORIZED_ACCESS");
+    } catch (error) {
+      expect(error).toEqual({
+        isDisplayMessage: true,
+        messageType: "[error]",
+        message: "UNAUTHORIZED_ACCESS",
       });
     }
   });
--- src/core/displayMessage.ts
+++ src/core/displayMessage.ts
@@ -1,3 +1,6 @@
-export function displayMessage(messageType: "error" | "warning" | "success", message: string): never {
+export function displayMessage(messageType: "error" | "warning" | "success" | (string & {}), message: string): never {
+  if (messageType === "error" || messageType === "warning" || messageType === "success") {
+    throw { isDisplayMessage: true, messageType: `[${messageType}]`, message };
+  }
   throw { isDisplayMessage: true, messageType, message };
 }
